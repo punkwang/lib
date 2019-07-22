62862d04a5481fdbf3e579e84d17953eee6078f4
punkwang <punkwang@qq.com>
2019-07-22 18:55:40 +0800

    Create short_url.php

---
+++ short_url.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ *
+ * Developed by Waizabú <code@waizabu.com>
+ *
+ *
+ */
+namespace eseperio\shortener;
+use eseperio\shortener\models\Shortener;
+use yii\base\Module;
+use yii\db\Expression;
+use yii\helpers\Url;
+/**
+ * Class ShortenerModule
+ * @package eseperio\shortener
+ */
+class ShortenerModule extends Module
+{
+    /**
+     *
+     * Must have a lenght of 61
+     * Default is ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789 in random order
+     * @var string
+     */
+    public $options = "NkAXITYvw3iObfKEaoeCUxqm2Dnrugl9PthVSM8yc7GQdBJHW6Lz4ZsjR15pF";
+    /**
+     * @var array
+     */
+    public $urlConfig = [
+        '<id:[\d\w]{4}>' => 'shortener/default/parse',
+    ];
+    /**
+     * @param $id
+     * @return mixed|string the url
+     */
+    public function expand($id)
+    {
+        $model = Shortener::find()
+            ->where(['shortened' => (new Expression("BINARY('$id')"))])
+            ->one();
+        if (!empty($model)) {
+//Delete record if is not valid anymore.
+            if (!is_null($model->valid_until) && time() > $model->valid_until) {
+                $model->delete();
+            } else {
+                return $model->url;
+            }
+        }
+        return false;
+    }
+    /**
+     * @param $url      string|array It accepts any url format allowed by Yii2
+     * @param $lifetime integer Time in seconds that the links must be available
+     */
+    public function short($url, $lifetime = null)
+    {
+        $model = new Shortener();
+        $model->setAttributes([
+            'url' => Url::to($url),
+            'valid_until' => empty($lifetime) ? null : time() + $lifetime
+        ]);
+        if ($model->save())
+            return $model->shortened;
+        return false;
+    }
+    /**
+     * @return string
+     */
+    public function getShortId()
+    {
+        return $this->generateShortId();
+    }
+    /**
+     * Method to generate short id of url
+     */
+    private function generateShortId()
+    {
+        $monthYear = +date('y') + +date('n');
+        $dayHour = +date('j') + +date('G');
+        $id = [
+            $this->options[$monthYear],
+            $this->options[$dayHour],
+            $this->options[+date('s')],
+            $this->options[+date('i')],
+        ];
+        return join("", $id);
+    }
+}
